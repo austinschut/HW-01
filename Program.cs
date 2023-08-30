b7d4872f6d90c4b55232250e2b632846185236e8
Austin Schut <adschut5@gmail.com>
2023-08-30 09:19:34 -0600

    FINAL PROGRAM

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Welcome to the programers gym where we exersise all our programing needs");
+Console.WriteLine("Press [Enter] five times fast");
+Console.WriteLine();
+DateTime started = DateTime.Now;
+Console.ReadLine();
+Console.ReadLine();
+Console.ReadLine();
+Console.ReadLine();
+DateTime finished = DateTime.Now;
+Console.WriteLine($"You did it in {finished - started}!");
